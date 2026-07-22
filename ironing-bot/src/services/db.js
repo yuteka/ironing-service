025b38dc95967c9d576dd13d063693b5e191c174
yuteka <yutekahema003@gmail.com>
2026-07-23 02:52:19 +0530

    Use Prisma Client Singleton pattern for Vercel Serverless environment

--- ironing-bot/src/services/db.js
+++ ironing-bot/src/services/db.js
@@ -2,12 +2,19 @@ const { PrismaClient } = require('@prisma/client');
 
 const fallbackDbUrl = "postgresql://postgres.gblktnmhkslpiirnkifl:MyIroningPass2026!@aws-0-ap-northeast-1.pooler.supabase.com:5432/postgres";
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL || fallbackDbUrl
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL || fallbackDbUrl
+      }
     }
-  }
-});
+  });
+};
+
+const globalForPrisma = global;
+const prisma = globalForPrisma.prisma || prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 module.exports = prisma;
